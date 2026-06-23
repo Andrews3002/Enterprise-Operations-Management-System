24ed2094e80570c46105750558584b5f006f2923
Andrews3002 <alexangeloandrews@gmail.com>
2026-06-23 04:13:29 -0400

    fix: removed dept_id foreign key from workflows and added it to workflow_stages itself

--- Tables/workflows.sql
+++ Tables/workflows.sql
@@ -1,7 +1,6 @@
 CREATE TABLE workflows (
     workflow_id NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     workflow_name VARCHAR2(100) NOT NULL UNIQUE,
-    dept_id NUMBER NOT NULL,
     description VARCHAR2(500),
     sla_hours NUMBER(5) NOT NULL,
     is_active NUMBER(1) DEFAULT 1 NOT NULL,
