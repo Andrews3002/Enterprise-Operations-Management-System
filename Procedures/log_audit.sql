91bf6ba09799f343d178ccad116b6046c37ddfa0
Andrews3002 <alexangeloandrews@gmail.com>
2026-06-23 03:44:24 -0400

    update: adding proceedure log_audit

---
+++ Procedures/log_audit.sql
@@ -0,0 +1,18 @@
+CREATE OR REPLACE PROCEDURE log_audit (
+    p_table_name IN VARCHAR2,
+    p_record_id IN NUMBER,
+    p_action IN VARCHAR2,
+    p_changed_by IN NUMBER,
+    p_old_value IN VARCHAR2 DEFAULT NULL,
+    p_new_value IN VARCHAR2 DEFAULT NULL
+) AS
+BEGIN
+    INSERT INTO audit_logs (
+        table_name, record_id, action,
+        changed_by, old_value, new_value, changed_at
+    ) VALUES (
+        p_table_name, p_record_id, p_action,
+        p_changed_by, p_old_value, p_new_value, SYSTIMESTAMP
+    );
+END log_audit;
+/
