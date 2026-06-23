db3ff2b915ad589ef3db662ec7b035a4f48c71c4
Andrews3002 <alexangeloandrews@gmail.com>
2026-06-23 03:35:43 -0400

    fix: rename index idx_decision_request to idx_decisions_request

--- Indexes/decisions.sql
+++ Indexes/decisions.sql
@@ -1,2 +1,2 @@
-CREATE INDEX idx_decision_request ON decisions(request_id);
+CREATE INDEX idx_decisions_request ON decisions(request_id);
 CREATE INDEX idx_decisions_decider ON decisions(decider_id);
