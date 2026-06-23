8b828beedad78f4bb43d63079ccb2daf94d2e07c
Andrews3002 <alexangeloandrews@gmail.com>
2026-06-23 06:55:45 -0400

    update: adding synonyms to other user dont have to use the ADMIN object every time to reference ADMIN schemas

---
+++ RBAC/Synonyms.sql
@@ -0,0 +1,6 @@
+CREATE PUBLIC SYNONYM workflow_pkg FOR ADMIN.workflow_pkg;
+CREATE PUBLIC SYNONYM incident_pkg FOR ADMIN.incident_pkg;
+CREATE PUBLIC SYNONYM reporting_pkg FOR ADMIN.reporting_pkg;
+CREATE PUBLIC SYNONYM department_spend FOR ADMIN.department_spend;
+CREATE PUBLIC SYNONYM assign_task FOR ADMIN.assign_task;
+CREATE PUBLIC SYNONYM audit_logs FOR ADMIN.audit_logs;
