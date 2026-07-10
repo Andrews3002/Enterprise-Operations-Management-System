9172a9fe688114b8448e19392249ff322048ffb8
Andrews3002 <alexangeloandrews@gmail.com>
2026-07-10 17:16:10 -0400

    fix: the trigger to update completed_at when a task is completed needed to use (IS NULL) instead of (= NULL)

--- Triggers/trg_task_complete_or_reopen.sql
+++ Triggers/trg_task_complete_or_reopen.sql
@@ -2,7 +2,7 @@ CREATE OR REPLACE TRIGGER trg_task_complete_or_reopen
 BEFORE UPDATE ON tasks
 FOR EACH ROW
 BEGIN
-    IF :NEW.status = 'DONE' AND :OLD.completed_at = NULL THEN
+    IF :NEW.status = 'DONE' AND :OLD.completed_at IS NULL THEN
         :NEW.completed_at := SYSTIMESTAMP;
     ELSIF :NEW.status = 'IN_PROGRESS' THEN
         :NEW.completed_at := NULL;
