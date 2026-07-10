984eed4f34997607cf3abb11562bdde3f08cca58
Andrews3002 <alexangeloandrews@gmail.com>
2026-07-10 10:50:36 -0400

    update: the trigger trg_update_department_manager_on_update now handles if a manager is made active again

--- Triggers/trg_update_department_manager_on_update.sql
+++ Triggers/trg_update_department_manager_on_update.sql
@@ -6,14 +6,14 @@ BEGIN
         UPDATE departments
         SET manager_id = null
         WHERE dept_id = :NEW.dept_id;
+    ELSIF :NEW.role_id = 3 AND :NEW.is_active = 1 THEN
+        UPDATE departments
+        SET manager_id = :NEW.emp_id
+        WHERE dept_id = :NEW.dept_id;
     ELSIF :OLD.role_id = 3 AND :NEW.role_id != 3 THEN
         UPDATE departments
         SET manager_id = NULL
         WHERE dept_id = :OLD.dept_id;
-    ELSIF :OLD.role_id != 3 AND :NEW.role_id = 3 THEN
-        UPDATE departments
-        SET manager_id = :NEW.emp_id
-        WHERE dept_id = :NEW.dept_id;
     END IF;
 END;
 /
