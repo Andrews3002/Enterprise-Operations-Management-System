89aaf5310eb19f0a342445045dad6e201939d5e1
Andrews3002 <alexangeloandrews@gmail.com>
2026-07-10 10:02:40 -0400

    update: updating the trg_update_department_manager_on_update trigger to pickup when a manager is no longer active

--- Triggers/trg_update_department_manager_on_update.sql
+++ Triggers/trg_update_department_manager_on_update.sql
@@ -2,11 +2,14 @@ CREATE OR REPLACE TRIGGER trg_update_department_manager_on_update
 AFTER UPDATE ON employees
 FOR EACH ROW
 BEGIN
-    IF :OLD.role_id = 3 AND :NEW.role_id != 3 THEN
+    IF :NEW.role_id = 3 AND :NEW.is_active = 0 THEN
+        UPDATE departments
+        SET manager_id = null
+        WHERE dept_id = :NEW.dept_id;
+    ELSIF :OLD.role_id = 3 AND :NEW.role_id != 3 THEN
         UPDATE departments
         SET manager_id = NULL
         WHERE dept_id = :OLD.dept_id;
-        
     ELSIF :OLD.role_id != 3 AND :NEW.role_id = 3 THEN
         UPDATE departments
         SET manager_id = :NEW.emp_id
