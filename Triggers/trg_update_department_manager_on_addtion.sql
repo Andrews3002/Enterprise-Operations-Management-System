d728660d3556754a694f1f4e6fe03a6436a0dd88
Andrews3002 <alexangeloandrews@gmail.com>
2026-07-10 08:50:23 -0400

    feature: adding a trigger to auto update the department manager when a new manager is added or someone is promoted to manager

---
+++ Triggers/trg_update_department_manager_on_addtion.sql
@@ -0,0 +1,10 @@
+CREATE OR REPLACE TRIGGER trg_update_department_manager_on_addtion
+BEFORE INSERT OR UPDATE ON employees
+FOR EACH ROW
+WHEN (NEW.role_id = 3)
+BEGIN
+    UPDATE departments
+    SET manager_id = :NEW.emp_id
+    WHERE dept_id = :NEW.dept_id;
+END;
+/
